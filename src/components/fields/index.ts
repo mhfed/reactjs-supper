27ef84701a4e8d420a74370440c837efecb7b7d6
duc.bui <duc.bui@novus-fintech.com>
2023-01-03 15:14:36 +0700

    build html css for create user segment

--- src/components/fields/index.ts
+++ src/components/fields/index.ts
@@ -1,5 +1,6 @@
 import InputField from './InputField';
 import PasswordField from './PasswordField';
 import SelectField from './SelectFiled';
+import AutocompleteAsyncField from './AutocompleteAsyncField';
 
-export { InputField, PasswordField, SelectField };
+export { InputField, PasswordField, SelectField, AutocompleteAsyncField };
