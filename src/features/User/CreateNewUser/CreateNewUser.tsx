572ec8125e760d2e2e5045dc6e056326827f79ac
@hieu.nguyen <hieu.nguyen@novus-fintech.com>
2023-01-03 17:54:51 +0700

    fix css layout form

--- src/features/User/CreateNewUser/CreateNewUser.tsx
+++ src/features/User/CreateNewUser/CreateNewUser.tsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { enqueueSnackbarAction } from 'actions/app.action';
 import { InputField, PasswordField, PreviewField, SelectField } from 'components/fields';
 import { useFormik } from 'formik';
-import { Button, Paper, Stack, Typography } from '@mui/material';
+import { Box, Button, Paper, Stack, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Trans } from 'react-i18next';
 import { validate } from 'helpers';
@@ -30,8 +30,9 @@ const useStyles = makeStyles((theme) => ({
   form: {
     width: '100%',
     height: '100%',
+    display: 'flex',
     flexDirection: 'column',
-    alignItems: 'flex-start',
+    justifyContent: 'space-between',
   },
 }));
 type CreateNewUserProps = {};
@@ -103,34 +104,38 @@ const CreateNewUser: React.FC<CreateNewUserProps> = () => {
   const renderContent = (stateForm: string) => {
     switch (stateForm) {
       case STATE_FORM.PREVIEW:
+        const site_name = SITE_NAME_OPTIONS.find((s) => s.value === values.site_name)?.label;
+        const status = USER_STATUS_OPTIONS.find((s) => s.value === values.status)?.label;
         return (
           <form className={classes.form} noValidate onSubmit={handleSubmit}>
-            <div className={classes.title}>
-              <Typography variant="h4">
-                <Trans>lang_user_details</Trans>
-              </Typography>
-            </div>
-            <Stack direction="row" sx={{ margin: '12px 0' }}>
-              <PreviewField sx={{ mb: 2, mr: 4 }} label="lang_full_name" value={values.full_name} />
-              <PreviewField label="lang_sitename" value={values.site_name} />
-            </Stack>
-            <Stack direction="row" sx={{ margin: '12px 0' }}>
-              <PreviewField sx={{ mb: 2, mr: 4 }} label="lang_user_login" value={values.user_login} />
-              <PreviewField label="lang_status" value={values.status} />
-            </Stack>
-            <Stack direction="row" sx={{ margin: '12px 0', width: '50%' }}>
-              <PreviewField sx={{ mb: 2, mr: 2 }} label="lang_password" value={values.user_login} />
-            </Stack>
-            <Stack direction="row" sx={{ margin: '12px 0', width: '50%' }}>
-              <PreviewField
-                sx={{ mb: 2, mr: 2 }}
-                variant="outlined"
-                label="lang_notes"
-                value={values.description}
-                multiline
-                rows={4}
-              />
-            </Stack>
+            <Box>
+              <div className={classes.title}>
+                <Typography variant="h4">
+                  <Trans>lang_user_details</Trans>
+                </Typography>
+              </div>
+              <Stack direction="row" sx={{ margin: '12px 0' }}>
+                <PreviewField sx={{ mb: 2, mr: 4 }} label="lang_full_name" value={values.full_name} />
+                <PreviewField label="lang_sitename" value={site_name} />
+              </Stack>
+              <Stack direction="row" sx={{ margin: '12px 0' }}>
+                <PreviewField sx={{ mb: 2, mr: 4 }} label="lang_user_login" value={values.user_login} />
+                <PreviewField label="lang_status" value={status} />
+              </Stack>
+              <Stack direction="row" sx={{ margin: '12px 0', width: '50%' }}>
+                <PreviewField sx={{ mb: 2, mr: 2 }} label="lang_password" value={values.user_login} />
+              </Stack>
+              <Stack direction="row" sx={{ margin: '12px 0', width: '50%' }}>
+                <PreviewField
+                  sx={{ mb: 2, mr: 2 }}
+                  variant="outlined"
+                  label="lang_notes"
+                  value={values.description}
+                  multiline
+                  rows={4}
+                />
+              </Stack>
+            </Box>
             <Stack direction="row" justifyContent="end" alignItems="center" spacing={2} sx={{ margin: '12px 0' }}>
               <Button variant="outlined" onClick={handleReturn}>
                 <Trans>lang_return</Trans>
@@ -145,106 +150,108 @@ const CreateNewUser: React.FC<CreateNewUserProps> = () => {
       default:
         return (
           <form className={classes.form} noValidate onSubmit={handleSubmit}>
-            <div className={classes.title}>
-              <Typography variant="h4">
-                <Trans>lang_user_details</Trans>
-              </Typography>
-            </div>
-            <Stack direction="row" sx={{ margin: '12px 0' }}>
-              <InputField
-                id="full_name"
-                name="full_name"
-                sx={{ mb: 2, mr: 4 }}
-                label="lang_full_name"
-                required
-                fullWidth
-                value={values.full_name}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.full_name && Boolean(errors.full_name)}
-                helperText={touched.full_name && errors.full_name}
-                inputProps={{ maxLength: 64 }}
-              />
-              <SelectField
-                options={SITE_NAME_OPTIONS}
-                name="site_name"
-                label="lang_sitename"
-                id="site_name"
-                fullWidth={true}
-                onBlur={handleBlur}
-                value={values.site_name}
-                onChange={handleChange}
-                error={touched.site_name && Boolean(errors.site_name)}
-                helperText={touched.site_name && errors.site_name}
-              />
-            </Stack>
-            <Stack direction="row" sx={{ margin: '12px 0' }}>
-              <InputField
-                id="user_login"
-                name="user_login"
-                sx={{ mb: 2, mr: 4 }}
-                label="lang_user_login"
-                required
-                fullWidth
-                value={values.user_login}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.user_login && Boolean(errors.user_login)}
-                helperText={touched.user_login && errors.user_login}
-                inputProps={{ maxLength: 255 }}
-              />
-              <SelectField
-                options={USER_STATUS_OPTIONS}
-                name="status"
-                label="lang_status"
-                id="status"
-                fullWidth={true}
-                onBlur={handleBlur}
-                value={values.status}
-                onChange={handleChange}
-                error={touched.status && Boolean(errors.status)}
-                helperText={touched.status && errors.status}
-              />
-            </Stack>
-            <Stack direction="row" sx={{ margin: '12px 0', width: '50%' }}>
-              <PasswordField
-                id="password"
-                name="password"
-                sx={{ mb: 2, mr: 2 }}
-                label="lang_password"
-                required
-                fullWidth
-                value={values.password}
-                onChange={(p: string) => setFieldValue('password', p)}
-                onBlur={handleBlur}
-                error={touched.password && Boolean(errors.password)}
-                generate
-                helperText={touched.password && errors.password}
-              />
-            </Stack>
-            <div className={classes.title}>
-              <Typography variant="h4">
-                <Trans>lang_notes</Trans>
-              </Typography>
-            </div>
-            <Stack direction="row" sx={{ margin: '12px 0', width: '50%' }}>
-              <InputField
-                id="description"
-                name="description"
-                sx={{ mb: 2, mr: 2 }}
-                label="lang_description"
-                required
-                fullWidth
-                value={values.description}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.description && Boolean(errors.description)}
-                helperText={touched.description && errors.description}
-                multiline
-                rows={4}
-                inputProps={{ maxLength: 255 }}
-              />
-            </Stack>
+            <Box>
+              <div className={classes.title}>
+                <Typography variant="h4">
+                  <Trans>lang_user_details</Trans>
+                </Typography>
+              </div>
+              <Stack direction="row" sx={{ margin: '12px 0' }}>
+                <InputField
+                  id="full_name"
+                  name="full_name"
+                  sx={{ mb: 2, mr: 4 }}
+                  label="lang_full_name"
+                  required
+                  fullWidth
+                  value={values.full_name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.full_name && Boolean(errors.full_name)}
+                  helperText={touched.full_name && errors.full_name}
+                  inputProps={{ maxLength: 64 }}
+                />
+                <SelectField
+                  options={SITE_NAME_OPTIONS}
+                  name="site_name"
+                  label="lang_sitename"
+                  id="site_name"
+                  fullWidth={true}
+                  onBlur={handleBlur}
+                  value={values.site_name}
+                  onChange={handleChange}
+                  error={touched.site_name && Boolean(errors.site_name)}
+                  helperText={touched.site_name && errors.site_name}
+                />
+              </Stack>
+              <Stack direction="row" sx={{ margin: '12px 0' }}>
+                <InputField
+                  id="user_login"
+                  name="user_login"
+                  sx={{ mb: 2, mr: 4 }}
+                  label="lang_user_login"
+                  required
+                  fullWidth
+                  value={values.user_login}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.user_login && Boolean(errors.user_login)}
+                  helperText={touched.user_login && errors.user_login}
+                  inputProps={{ maxLength: 255 }}
+                />
+                <SelectField
+                  options={USER_STATUS_OPTIONS}
+                  name="status"
+                  label="lang_status"
+                  id="status"
+                  fullWidth={true}
+                  onBlur={handleBlur}
+                  value={values.status}
+                  onChange={handleChange}
+                  error={touched.status && Boolean(errors.status)}
+                  helperText={touched.status && errors.status}
+                />
+              </Stack>
+              <Stack direction="row" sx={{ margin: '12px 0', width: '50%' }}>
+                <PasswordField
+                  id="password"
+                  name="password"
+                  sx={{ mb: 2, mr: 2 }}
+                  label="lang_password"
+                  required
+                  fullWidth
+                  value={values.password}
+                  onChange={(p: string) => setFieldValue('password', p)}
+                  onBlur={handleBlur}
+                  error={touched.password && Boolean(errors.password)}
+                  generate
+                  helperText={touched.password && errors.password}
+                />
+              </Stack>
+              <div className={classes.title}>
+                <Typography variant="h4">
+                  <Trans>lang_notes</Trans>
+                </Typography>
+              </div>
+              <Stack direction="row" sx={{ margin: '12px 0', width: '50%' }}>
+                <InputField
+                  id="description"
+                  name="description"
+                  sx={{ mb: 2, mr: 2 }}
+                  label="lang_description"
+                  required
+                  fullWidth
+                  value={values.description}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.description && Boolean(errors.description)}
+                  helperText={touched.description && errors.description}
+                  multiline
+                  rows={4}
+                  inputProps={{ maxLength: 255 }}
+                />
+              </Stack>
+            </Box>
             <Stack direction="row" justifyContent="end" alignItems="center" spacing={2} sx={{ margin: '12px 0' }}>
               <Button variant="outlined" onClick={handleClearData}>
                 <Trans>lang_clear_data</Trans>
