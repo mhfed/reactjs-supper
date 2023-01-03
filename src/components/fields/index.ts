4b1f4e2d652b3ae891bcd1c2bfb5ca713ab8c28f
@hieu.nguyen <hieu.nguyen@novus-fintech.com>
2023-01-03 14:18:05 +0700

    Create PreviewField & logic n ext page

--- src/components/fields/index.ts
+++ src/components/fields/index.ts
@@ -1,5 +1,6 @@
 import InputField from './InputField';
 import PasswordField from './PasswordField';
 import SelectField from './SelectFiled';
+import PreviewField from './PreviewField';
 
-export { InputField, PasswordField, SelectField };
+export { InputField, PasswordField, SelectField, PreviewField };
