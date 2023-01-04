e0cfb83346ed0e6cb4aa05cda7788f0021f4b3ee
duc.bui <duc.bui@novus-fintech.com>
2023-01-04 11:01:56 +0700

    create the previewpage for create new segment

--- src/components/fields/AutocompleteAsyncField.tsx
+++ src/components/fields/AutocompleteAsyncField.tsx
@@ -6,7 +6,6 @@ import { Trans } from 'react-i18next';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import { getListSubscriberSegmenttUrl } from 'apis/request.url';
-import { debounce } from '@mui/material/utils';
 
 type Options = {
   label: string;
@@ -32,7 +31,8 @@ type AutocompleteAsyncFieldProps = {
 };
 
 const AutocompleteAsyncField: React.FC<AutocompleteAsyncFieldProps> = ({ label, helperText, value, setFieldValue, ...props }) => {
-  // const [options, setOptions] = React.useState([]);
+  const [options, setOptions] = React.useState([]);
+  const timeoutId = React.useRef<number | null>(null);
   function _renderHelperText() {
     if (props.error) {
       return (
@@ -46,6 +46,7 @@ const AutocompleteAsyncField: React.FC<AutocompleteAsyncFieldProps> = ({ label, 
   }
   const getData = async (SearchKey: string) => {
     try {
+      if (SearchKey.length < 2) return;
       const response: any = await httpRequest.get(
         getListSubscriberSegmenttUrl({
           pageId: 1,
@@ -53,7 +54,11 @@ const AutocompleteAsyncField: React.FC<AutocompleteAsyncFieldProps> = ({ label, 
           searchText: SearchKey,
         }),
       );
-      console.log('du lieu tra ve tu api search la:', response);
+      if (!response.data) {
+        setOptions([]);
+      } else {
+        setOptions(response.data);
+      }
     } catch (error) {
       console.log('Search error:', error);
     }
@@ -64,7 +69,10 @@ const AutocompleteAsyncField: React.FC<AutocompleteAsyncFieldProps> = ({ label, 
   };
   const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log('character in autocomplete box is:', e.target.value);
-    getData(e.target.value);
+    timeoutId.current && window.clearTimeout(timeoutId.current);
+    timeoutId.current = window.setTimeout(() => {
+      getData(e.target.value);
+    }, 400);
   };
   return (
     <FormControl required sx={{ minWidth: 120, width: props.fullWidth ? '100%' : '' }}>
@@ -73,9 +81,9 @@ const AutocompleteAsyncField: React.FC<AutocompleteAsyncFieldProps> = ({ label, 
         multiple
         id={props.id}
         onChange={handleChange}
-        options={top100Films}
-        getOptionLabel={(option) => option.title}
-        renderOption={(props, option, { selected }) => <li {...props}>{option.title}</li>}
+        options={options}
+        getOptionLabel={(option) => option.username + ' (' + option.site_name + ')'}
+        // renderOption={(props, option, { selected }) => <li {...props}>{option.title}</li>}
         renderInput={(params) => (
           <TextField
             required={props.required}
@@ -93,129 +101,3 @@ const AutocompleteAsyncField: React.FC<AutocompleteAsyncFieldProps> = ({ label, 
 };
 
 export default AutocompleteAsyncField;
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-  { title: 'Casablanca', year: 1942 },
-  { title: 'City Lights', year: 1931 },
-  { title: 'Psycho', year: 1960 },
-  { title: 'The Green Mile', year: 1999 },
-  { title: 'The Intouchables', year: 2011 },
-  { title: 'Modern Times', year: 1936 },
-  { title: 'Raiders of the Lost Ark', year: 1981 },
-  { title: 'Rear Window', year: 1954 },
-  { title: 'The Pianist', year: 2002 },
-  { title: 'The Departed', year: 2006 },
-  { title: 'Terminator 2: Judgment Day', year: 1991 },
-  { title: 'Back to the Future', year: 1985 },
-  { title: 'Whiplash', year: 2014 },
-  { title: 'Gladiator', year: 2000 },
-  { title: 'Memento', year: 2000 },
-  { title: 'The Prestige', year: 2006 },
-  { title: 'The Lion King', year: 1994 },
-  { title: 'Apocalypse Now', year: 1979 },
-  { title: 'Alien', year: 1979 },
-  { title: 'Sunset Boulevard', year: 1950 },
-  {
-    title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    year: 1964,
-  },
-  { title: 'The Great Dictator', year: 1940 },
-  { title: 'Cinema Paradiso', year: 1988 },
-  { title: 'The Lives of Others', year: 2006 },
-  { title: 'Grave of the Fireflies', year: 1988 },
-  { title: 'Paths of Glory', year: 1957 },
-  { title: 'Django Unchained', year: 2012 },
-  { title: 'The Shining', year: 1980 },
-  { title: 'WALL·E', year: 2008 },
-  { title: 'American Beauty', year: 1999 },
-  { title: 'The Dark Knight Rises', year: 2012 },
-  { title: 'Princess Mononoke', year: 1997 },
-  { title: 'Aliens', year: 1986 },
-  { title: 'Oldboy', year: 2003 },
-  { title: 'Once Upon a Time in America', year: 1984 },
-  { title: 'Witness for the Prosecution', year: 1957 },
-  { title: 'Das Boot', year: 1981 },
-  { title: 'Citizen Kane', year: 1941 },
-  { title: 'North by Northwest', year: 1959 },
-  { title: 'Vertigo', year: 1958 },
-  {
-    title: 'Star Wars: Episode VI - Return of the Jedi',
-    year: 1983,
-  },
-  { title: 'Reservoir Dogs', year: 1992 },
-  { title: 'Braveheart', year: 1995 },
-  { title: 'M', year: 1931 },
-  { title: 'Requiem for a Dream', year: 2000 },
-  { title: 'Amélie', year: 2001 },
-  { title: 'A Clockwork Orange', year: 1971 },
-  { title: 'Like Stars on Earth', year: 2007 },
-  { title: 'Taxi Driver', year: 1976 },
-  { title: 'Lawrence of Arabia', year: 1962 },
-  { title: 'Double Indemnity', year: 1944 },
-  {
-    title: 'Eternal Sunshine of the Spotless Mind',
-    year: 2004,
-  },
-  { title: 'Amadeus', year: 1984 },
-  { title: 'To Kill a Mockingbird', year: 1962 },
-  { title: 'Toy Story 3', year: 2010 },
-  { title: 'Logan', year: 2017 },
-  { title: 'Full Metal Jacket', year: 1987 },
-  { title: 'Dangal', year: 2016 },
-  { title: 'The Sting', year: 1973 },
-  { title: '2001: A Space Odyssey', year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: 'Toy Story', year: 1995 },
-  { title: 'Bicycle Thieves', year: 1948 },
-  { title: 'The Kid', year: 1921 },
-  { title: 'Inglourious Basterds', year: 2009 },
-  { title: 'Snatch', year: 2000 },
-  { title: '3 Idiots', year: 2009 },
-  { title: 'Monty Python and the Holy Grail', year: 1975 },
-];
