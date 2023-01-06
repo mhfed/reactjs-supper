7a41d1680a4bb5c63543a3aeac4a5a98a3ca5a8f
phong.dang <phong.dang@novus-fintech.com>
2023-01-06 14:17:07 +0700

    update modal custom header

--- src/components/atoms/ButtonBase.tsx
+++ src/components/atoms/ButtonBase.tsx
@@ -1,3 +1,11 @@
+/*
+ * Created on Fri Jan 06 2023
+ *
+ * Base button with connection check
+ *
+ * Copyright (c) 2023 - Novus Fintech
+ */
+
 import React from 'react';
 import Button, { ButtonProps } from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
