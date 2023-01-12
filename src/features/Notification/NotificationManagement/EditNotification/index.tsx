1e3ae7699c7757ac3d65b3e056cd75e733ad5931
pham dac tai <tai.pham@quant-edge.com>
2023-01-12 18:20:13 +0700

    CMS-274 change message

--- src/features/Notification/NotificationManagement/EditNotification/index.tsx
+++ src/features/Notification/NotificationManagement/EditNotification/index.tsx
@@ -315,20 +315,22 @@ const validationSchema = yup.object().shape({
       ? schema.min(1, 'lang_select_segment_subcriber').required('lang_select_segment_subcriber')
       : schema;
   }),
-  title: yup.string().required('lang_title_required').max(64, 'lang_validate_title'),
-  message: yup.string().required('lang_message_required').max(192, 'lang_validate_message'),
+  title: yup.string().required('lang_please_enter_title').max(64, 'lang_validate_title'),
+  message: yup.string().required('lang_please_enter_message').max(192, 'lang_validate_message'),
   schedule: yup.string().when(['delivery_type', 'notification_type'], {
     is: (delivery_type: 'Instant' | 'Schedule', notification_type: Notification_Type) => {
       return delivery_type === DELIVERY_TYPE.Schedule && notification_type === NOTIFICATION_TYPE.Direct;
     },
-    then: yup.string().required('lang_schedule_time_required').compareTimes(),
+    then: yup.string().required('lang_please_select_schedule_time').compareTimes(),
     // .checkValidField('lang_schedule_time_required'),
   }),
   segment: yup.mixed().when('notification_type', (value, schema) => {
-    return value === NOTIFICATION_TYPE.Segment ? schema.required('lang_field_required') : schema;
+    return value === NOTIFICATION_TYPE.Segment ? schema.required('lang_please_select_segment') : schema;
   }),
   sitename: yup.array().when('notification_type', (value, schema) => {
-    return value === NOTIFICATION_TYPE.Sitename ? schema.min(1, 'lang_field_required').required('lang_field_required') : schema;
+    return value === NOTIFICATION_TYPE.Sitename
+      ? schema.min(1, 'lang_please_select_sitename').required('lang_please_select_sitename')
+      : schema;
   }),
   type_url: yup.string().required('lang_url_require'),
 });
