0a18891c05e9c1988cc03f8e09ff4386517c244b
duc.bui <duc.bui@novus-fintech.com>
2023-01-16 10:02:39 +0700

    add trim() for title and message, bug cms-270

--- src/features/Notification/CreateNewNotification/CreateNewNotification.tsx
+++ src/features/Notification/CreateNewNotification/CreateNewNotification.tsx
@@ -270,9 +270,9 @@ const validationSchema = yup.object().shape({
       ? schema.min(1, 'lang_select_segment_subcriber').required('lang_select_segment_subcriber')
       : schema;
   }),
-  title: yup.string().required('lang_please_enter_title').max(64, 'lang_validate_title'),
-  message: yup.string().required('lang_please_enter_message').max(192, 'lang_validate_message'),
-  expire: yup.string().required('lang_enter_expire'),
+  title: yup.string().trim().required('lang_please_enter_title').max(64, 'lang_validate_title'),
+  message: yup.string().trim().required('lang_please_enter_message').max(192, 'lang_validate_message'),
+  expire: yup.string().trim().required('lang_enter_expire'),
   schedule: yup.string().when(['delivery_type', 'notification_type'], {
     is: (delivery_type: 'Instant' | 'Schedule', notification_type: Notification_Type) => {
       return delivery_type === DELIVERY_TYPE.Schedule && notification_type === NOTIFICATION_TYPE.Direct;
