66c6b2607f8ba430ba3274584d3750b29aca68e7
@hieu.nguyen <hieu.nguyen@novus-fintech.com>
2023-02-01 15:23:51 +0700

    /CMS-419 show there is no change..

--- src/features/Articles/CreateNewArticles/ArticlesEditForm.tsx
+++ src/features/Articles/CreateNewArticles/ArticlesEditForm.tsx
@@ -35,6 +35,7 @@ import httpRequest from 'services/httpRequest';
 import { useDispatch } from 'react-redux';
 import { enqueueSnackbarAction } from 'actions/app.action';
 import { isBlobFile } from 'helpers';
+import { diff } from 'deep-diff';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -69,53 +70,64 @@ const ArticlesEditForm: React.FC<ArticlesEditFormProps> = ({ data: initValues, o
    * @param values Form value
    */
   const handleFormSubmit = async (values: LooseObject) => {
-    try {
-      const body: ICreateArticlesBody = {
-        subject: values.subject,
-        content: values.content,
-        site_name:
-          values.site_name === SITENAME.CUSTOM ? values.sitename_custom.map((e: any) => e.site_name) : [values.site_name],
-        securities: values.securities.map((e: any) => e.securities),
-        security_type: values.security_type,
-      };
-      if (values.image.url) {
-        body.image = values.image.url;
-      }
-      if (values.file.url) {
-        body.attachment_url = values.file.url;
-        body.attachment_name = values.file.name || 'attachment_name';
-      }
-      if (isBlobFile(values.image)) {
-        const formData = new FormData();
-        formData.append('file', values.image.file);
-        const { data: imageResponse } = await httpRequest.post(getUploadUrl(), formData);
-        body.image = imageResponse.url;
-      }
-      if (isBlobFile(values.file)) {
-        const formData = new FormData();
-        formData.append('file', values.file.file);
-        const { data: fileResponse } = await httpRequest.post(getUploadUrl(), formData);
-        body.attachment_url = fileResponse.url;
-        body.attachment_name = values.file.name;
-      }
-      await httpRequest.put(getArticlesUrl(initValues.article_id), body);
+    if (!diff(values, initValues)) {
       dispatch(
         enqueueSnackbarAction({
-          message: 'lang_update_articles_successfully',
+          message: 'lang_there_is_no_change_in_the_article_information',
           key: new Date().getTime() + Math.random(),
-          variant: 'success',
+          variant: 'warning',
         }),
       );
-      onSuccess?.();
       hideModal();
-    } catch (error) {
-      dispatch(
-        enqueueSnackbarAction({
-          message: 'lang_update_articles_unsuccessfully',
-          key: new Date().getTime() + Math.random(),
-          variant: 'error',
-        }),
-      );
+    } else {
+      try {
+        const body: ICreateArticlesBody = {
+          subject: values.subject,
+          content: values.content,
+          site_name:
+            values.site_name === SITENAME.CUSTOM ? values.sitename_custom.map((e: any) => e.site_name) : [values.site_name],
+          securities: values.securities.map((e: any) => e.securities),
+          security_type: values.security_type,
+        };
+        if (values.image.url) {
+          body.image = values.image.url;
+        }
+        if (values.file.url) {
+          body.attachment_url = values.file.url;
+          body.attachment_name = values.file.name || 'attachment_name';
+        }
+        if (isBlobFile(values.image)) {
+          const formData = new FormData();
+          formData.append('file', values.image.file);
+          const { data: imageResponse } = await httpRequest.post(getUploadUrl(), formData);
+          body.image = imageResponse.url;
+        }
+        if (isBlobFile(values.file)) {
+          const formData = new FormData();
+          formData.append('file', values.file.file);
+          const { data: fileResponse } = await httpRequest.post(getUploadUrl(), formData);
+          body.attachment_url = fileResponse.url;
+          body.attachment_name = values.file.name;
+        }
+        await httpRequest.put(getArticlesUrl(initValues.article_id), body);
+        dispatch(
+          enqueueSnackbarAction({
+            message: 'lang_update_articles_successfully',
+            key: new Date().getTime() + Math.random(),
+            variant: 'success',
+          }),
+        );
+        onSuccess?.();
+        hideModal();
+      } catch (error) {
+        dispatch(
+          enqueueSnackbarAction({
+            message: 'lang_update_articles_unsuccessfully',
+            key: new Date().getTime() + Math.random(),
+            variant: 'error',
+          }),
+        );
+      }
     }
   };
 
