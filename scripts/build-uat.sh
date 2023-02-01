e3535b2e2b3db9dac9b2ddbd686e1d3a554027fa
phong.dang <phong.dang@novus-fintech.com>
2023-02-01 15:00:09 +0700

    add script build uat and prod

---
+++ scripts/build-uat.sh
@@ -0,0 +1,6 @@
+echo "console.log('%c$(date +"%H:%M %d/%m/%Y")', 'color: #0f0;font-size:36px')" > public/build-version.js
+rm -rf build/
+
+nextBundle='iress-wealth-management-uat'
+currentBundle='iress-wealth-management-dev'
+sed -i -e "s/$currentBundle/$nextBundle/g" .firebaserc
