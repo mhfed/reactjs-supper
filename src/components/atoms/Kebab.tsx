bbdb46c4699d9a4ce146aa999798feca11808ac0
taiPham <tai.pham@novus-fintech.com>
2023-05-13 09:26:22 +0700

    add menu active.

--- src/components/atoms/Kebab.tsx
+++ src/components/atoms/Kebab.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
   icon: {
     padding: 2,
     background: theme.palette.background.iconButton,
-    '&:hover': {
+    '&:hover,&[aria-expanded="true"]': {
       backgroundColor: theme.palette.mode === 'dark' ? '#08D98D' : '#E1FFF4',
       color: theme.palette.mode === 'dark' ? '#FFFFFF' : '#08D98D',
     },
