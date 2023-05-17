e84a14358196758f080f6e53eff99a7bd2ea01f6
phong.dang <phong.dang@novus-fintech.com>
2023-05-17 15:02:22 +0700

    update body edit article

--- src/layouts/MainLayout/NavBar/Version.tsx
+++ src/layouts/MainLayout/NavBar/Version.tsx
@@ -5,7 +5,7 @@ const Version = () => {
   return (
     <>
       <Typography variant="caption" style={{ color: '#758695', padding: '8px 0 8px 24px' }}>
-        <Trans values={{ time: 'BUILD_TIME' }}>lang_build_version</Trans>
+        <Trans values={{ time: 'May 17 2023 14:45:17' }}>lang_build_version</Trans>
       </Typography>
     </>
   );
