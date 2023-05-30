d15d59a026cc09d02f4edd20a4cca2b002730713
hieu nguyen <hieu.nguyen@novus-fintech.com>
2023-05-30 17:06:54 +0700

    promise & async awati

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 
-//==================== Callback function  ================//
-// Là tham số - nhưng mà tham số này là function nên người ta gọi là callback function
-const nums = [1, 2, 3, 4, 5];
+//==================== Promise  ================//
 
-const callback = (item, index) => {
-    console.log(`STT ${index} la ${item}`);
+const promiseFunction = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hello")
+        })
+    })
 }
 
-nums.forEach(callback)
+const value = promiseFunction()
+    .then((value) => {
+        return value
+    }).catch((error) => {
+        return error
+    }).finally(() => {
+        return 'finally'
+    })
 
-//==================== Currying function ================//
-// Đơn giản là function return về function
+promiseFunction()
 
-function findNumber(num) {
-    return function (func) {
-        const result = [];
-        for (let i = 0; i < num; i++) {
-            if (func(i)) {
-                result.push[i]
-            }
-        }
-        return result;
+const p2 = value
+    .then((res) => {
+        return res
+    }).catch((error) => {
+        return error
+    }).finally(() => {
+        return 'finally'
+    })
+
+
+/**
+ * await chỉ được sử dungn trong 1 async function
+ */
+const handle = async () => {
+    try {
+        const response = await p2;
+        console.log({ response });
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log('2 finally');
     }
 }
+handle()
 
-const value = findNumber(10)(num => num % 2 === 0);
-console.log(value);
+    ; (async function () {
+        try {
+            const response = await p2;
+            console.log({ response });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            console.log('2 finally');
+        }
+    })();
