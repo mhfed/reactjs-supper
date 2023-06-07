27bd4098a91da18457d74f5b9920488d8d838008
hieu nguyen <hieu.nguyen@novus-fintech.com>
2023-06-07 17:43:19 +0700

    learn loader css, scss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import getText from "./ultil";
+import "./styles/style.css"
+import "./styles/style.scss"
 
 console.log(getText());
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,20 @@ console.log('__dirname', __dirname);
 console.log('path.resolve()', path.resolve());
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         app: path.resolve('src/index.js')
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "[name].js"// cái này optional vì trên entry có key là tên file đầu ra rồi,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss|css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
     }
 }
