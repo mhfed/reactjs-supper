d4f4435777d1f6ab913f11369acc8b4e3a4c746d
mhfed <nmhieu04091999@gmail.com>
2023-06-08 00:34:59 +0700

    learn sourcemap for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,49 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-console.log('__dirname', __dirname);
-console.log('path.resolve()', path.resolve());
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+console.log("__dirname", __dirname);
+console.log("path.resolve()", path.resolve());
 
-module.exports = {
-    mode: 'production',
+module.exports = (env) => {
+  const isDevelopment = Boolean(env.development);
+  return {
+    mode: isDevelopment ? "development" : "production",
     entry: {
-        app: path.resolve('src/index.js')
+      app: path.resolve("src/index.js"),
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].[contenthash].js",// cái này optional vì trên entry có key là tên file đầu ra rồi,
-        clean: true
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[contenthash].js", // cái này optional vì trên entry có key là tên file đầu ra rồi,
+      clean: true,
     },
+    devtool: isDevelopment ? "source-map" : false,
     module: {
-        rules: [
-            {
-                test: /\.s[ac]ss|css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            }
-        ]
+      rules: [
+        {
+          test: /\.s[ac]ss|css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css"
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Hoc Webpack App',
-            filename: 'index.html',
-            template: './src/template.html'
-        })
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+      }),
+      new HtmlWebpackPlugin({
+        title: "Hoc Webpack App",
+        filename: "index.html",
+        template: "./src/template.html",
+      }),
     ],
     devServer: {
-        static : {
-            directory: 'dist' // Đường dẫn tương đối đến thư mục chứa file html
-        },
-        port: 3000, // Port thay cho port mặc định 8080
-        open: true, // Mở trang webpack khi chạy terminal
-        hot: true, // Bật tính năng reload nhanh Hot Module Replacement
-        compress: true, // Bật Gzip cho các tài nguyên
-        historyApiFallback: true // Set true nếu đang dùng cho các SPA sử dụng history router
-    }
-}
+      static: {
+        directory: "dist", // Đường dẫn tương đối đến thư mục chứa file html
+      },
+      port: 3000, // Port thay cho port mặc định 8080
+      open: true, // Mở trang webpack khi chạy terminal
+      hot: true, // Bật tính năng reload nhanh Hot Module Replacement
+      compress: true, // Bật Gzip cho các tài nguyên
+      historyApiFallback: true, // Set true nếu đang dùng cho các SPA sử dụng history router
+    },
+  };
+};
